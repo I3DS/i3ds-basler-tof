240bca69587257efd99b320aa9e78f6fed6b424f
Espen Helle <espen.helle@sintef>
2018-10-09 16:40:45 +0200

    Fixes typo in parameter decoding

--- src/i3ds_basler_tof.cpp
+++ src/i3ds_basler_tof.cpp
@@ -70,7 +70,7 @@ int main ( int argc, char **argv )
         return -1;
     }
 
-    if ( vm.count ( "quite" ) )
+    if ( vm.count ( "quiet" ) )
     {
         logging::core::get()->set_filter ( logging::trivial::severity >= logging::trivial::warning );
     }
